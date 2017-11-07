d1a4f35488ee00ae0f50f52df315751702c78494
Sangita Jadhav <Sangita.Jadhav@harbingergroup.com>
2017-11-07 15:00:16 +0530

    create componet with props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from './containers/Header.jsx'
+
 class App extends React.Component{
     constructor(props){
         super(props);
 
          this.state =  {
            data: false,
+           header:'Header  from props',
+           content:'Content from props'
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -21,9 +25,34 @@ class App extends React.Component{
               <h1> Hello / Welcome</h1>
                <h1>{this.state.data  === false ? 'Logging out...' : 'Logging in...'}</h1>
              <button onClick = {this.handleClick}>{this.state.data  === false ? 'Log Out' : 'Log In'}</button>
+             <br/> <br/>
+             <Header headerProp={this.state.header}></Header>
+             <Content contentProp={this.state.content}></Content>
+
+
             </div>
         );  
     }
 } 
+
+// class Header extends  React.Component{
+//     render(){
+//         return(
+//             <div>
+//             <h1>{this.props.headerProp}</h1>
+//             </div>
+//         );
+//     }
+// }
+
+class Content extends React.Component{
+    render(){
+        return(
+            <div>
+             <h2>{this.props.contentProp}</h2>
+             </div>
+        );
+    }
+}
 
 export default App;
